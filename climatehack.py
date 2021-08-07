f295ca9c37ad6899b6ad3676fc9a4b3f0e178999
midnight-poet <86426844+midnight-poet@users.noreply.github.com>
2021-08-07 17:52:47 -0400

    Update climatehack.py

--- climatehack.py
+++ climatehack.py
@@ -8,6 +8,16 @@ DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+import pygame
+import random
+import pygame, sys
+pygame.init()
+
+#variables
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 600
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption("Climate Escape!")
@@ -33,6 +43,10 @@ def button(text, x, y, width, height, size, icolour, acolour, action = None):
     textRect.center = (x+(width/2), y+(height/2))
     gameDisplay.blit(textSurf, textRect)
 
+def makeroom():
+    pygame.draw.rect(gameDisplay, WHITE,(35, 50, DISPLAY_WIDTH-70, DISPLAY_HEIGHT-100)) 
+    pygame.draw.rect(gameDisplay, WHITE,(50, 35, DISPLAY_WIDTH-100, DISPLAY_HEIGHT-70)) 
+
 def gameLoop():
      rect_x = (DISPLAY_WIDTH * 0.45)
      
@@ -40,37 +54,37 @@ def quitGame():
     pygame.quit()
     sys.exit()
     quit()
+    
+def textObjects(text, font, colour):
+    textSurface = font.render(text, True, colour)
+    #returns text + rectangle
+    return textSurface, textSurface.get_rect()
 
 #main display
 def menu():
      #Declaration of variables    
     gameExit = True
-   
+    makeroom()
     while gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 gameExit = False
                 quit()
-        gameDisplay.fill(BLACK)
+       # gameDisplay.fill(BLACK)
 
         #Displays name
         largeText = pygame.font.Font('freesansbold.ttf', 50)
-        textSurf, textRect = textObjects("CLIMATE ESCAPE", largeText, BRIGHT_YELLOW)   
+        textSurf, textRect = textObjects("CLIMATE ESCAPE", largeText, BLACK)   
         textRect.center = (400, 300)
         gameDisplay.blit(textSurf, textRect)
 
 
-        largeText = pygame.font.Font('freesansbold.ttf', 20)
-        textSurf, textRect = textObjects("WINS: "+str(wins. count('1')), largeText, BRIGHT_GREEN)   
-        textRect.center = (200, 300)
-        gameDisplay.blit(textSurf, textRect)
+       # largeText = pygame.font.Font('freesansbold.ttf', 20)
+       # textSurf, textRect = textObjects("Test", largeText, BLACK)   
+       # textRect.center = (200, 300)
+       # gameDisplay.blit(textSurf, textRect)
 
-
-        #Buttons to quit, start, and how to play the game
-        button("QUIT", 500, 350, 200, 50, 20, BRIGHT_RED, RED, quitGame)      
-        button("START", 100, 350, 200, 50, 20, BRIGHT_GREEN, GREEN, gameLoop)
-        button("HOW TO PLAY", 300, 350, 200, 50, 20, BRIGHT_YELLOW, YELLOW, instructions)
  
         pygame.display.update()
         clock.tick(60)
